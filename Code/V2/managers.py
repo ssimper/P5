eee0e421d023f6c375d3c26b3d23b734432e35cf
ssimper <steph.simper@gmail.com>
2020-08-30 23:00:23 +0200

    sixth commit

---
+++ Code/V2/managers.py
@@ -0,0 +1,25 @@
+"""Création des Managers"""
+
+class ArticleManager:
+
+	def save_article(self, article):
+		pass
+
+	def get_by_id(self, id): #retourne un article par rapport à un id
+		pass
+
+	def get_all_by_category(self, category):
+		pass
+
+class CategoryManager:
+
+	def save_category(self, category):
+		pass
+
+	def get_by_id(self, id):
+		pass
+
+class ProductManager:
+
+	def save_product(self, product):
+		pass
