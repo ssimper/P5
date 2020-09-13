67f4a027548041e782491dadf6c248f170906053
ssimper <steph.simper@gmail.com>
2020-09-13 22:44:40 +0200

    Mise à jour 01 de la branche

---
+++ Code/V2/database2.py
@@ -0,0 +1,18 @@
+import mysql.connector
+import records
+
+class MysqlConnect:
+
+	def database_connect():
+		cnx = mysql.connector.connect(
+			user='simper',
+			password='eugostodomysql',
+			host='localhost',
+			database='off_test',
+			charset='utf8'
+			)
+		
+		cursor = cnx.cursor()
+		return cnx, cursor
+
+db = records.Database("mysql://simper:eugostodomysql@localhost/off_test")
--- Code/V2/managers.py
+++ Code/V2/managers.py
@@ -1,25 +1,31 @@
 """Création des Managers"""
+from database import db
 
-class ArticleManager:
+class ProductManager:
 
-	def save_article(self, article):
+	def save_product(self, product):
 		pass
 
-	def get_by_id(self, id): #retourne un article par rapport à un id
+	def get_by_id(self, bar_code): #retourne un produit par rapport à un code barre
+		rows = db.query("SELECT * FROM product WHERE bar_code='9002515601018'")
+		print(rows)
+
+	def get_all_by_category(self, category): #retourne tous les produits par rapport à une catégorie
 		pass
 
-	def get_all_by_category(self, category):
-		pass
+	def show_all_products(self, product):
+		rows = db.query("SELECT * FROM product")
+		for row in rows:
+			print(row['name'])
+
 
 class CategoryManager:
 
 	def save_category(self, category):
 		pass
 
-	def get_by_id(self, id):
+	def show_category(self, category):
 		pass
 
-class ProductManager:
-
-	def save_product(self, product):
+	def get_by_id(self, id):
 		pass
